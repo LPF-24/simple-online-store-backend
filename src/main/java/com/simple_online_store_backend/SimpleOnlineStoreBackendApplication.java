7815a07a56794e8087d5957f042f88e0d386b31a
max <mwlosev@gmail.com>
2025-03-14 09:24:28 +0300

    first commit

---
+++ src/main/java/com/simple_online_store_backend/SimpleOnlineStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simple_online_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOnlineStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOnlineStoreBackendApplication.class, args);
+	}
+
+}
