f350e8b328eccc4472e9932eca5b91e55d6048ef
max <mwlosev@gmail.com>
2025-03-14 09:24:28 +0300

    first commit

---
+++ src/test/java/com/simple_online_store_backend/SimpleOnlineStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple_online_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOnlineStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
