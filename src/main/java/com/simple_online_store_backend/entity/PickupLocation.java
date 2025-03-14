c7798a8d2bfac9a0e46eff27f682c1e28af27bb8
max <mwlosev@gmail.com>
2025-03-14 15:44:42 +0300

    New entities added

---
+++ src/main/java/com/simple_online_store_backend/entity/PickupLocation.java
@@ -0,0 +1,31 @@
+package com.simple_online_store_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "pickup_location")
+@NoArgsConstructor
+@Getter
+@Setter
+public class PickupLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    @Column(nullable = false, length = 100)
+    private String street;
+
+    @Column(name = "house_number", nullable = false, length = 10)
+    private String houseNumber;
+
+    @OneToMany(mappedBy = "pickupLocation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Order> orderList;
+}
