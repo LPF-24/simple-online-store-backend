db832f67714d417879d637548acfaa92a69616d6
max <mwlosev@gmail.com>
2025-03-14 13:18:00 +0300

    Little changes for application.yml

--- src/main/java/com/simple_online_store_backend/entity/User.java
+++ src/main/java/com/simple_online_store_backend/entity/User.java
@@ -1,4 +1,31 @@
 package com.simple_online_store_backend.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String userName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "phone_number", length = 10)
+    private String phoneNumber;
+
+    @Column(length = 50)
+    private String email;
 }
