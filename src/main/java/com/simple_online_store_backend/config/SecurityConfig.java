e3d8b238d259f1e21f2475dfc67476f3a9f9fcad
max <mwlosev@gmail.com>
2025-03-15 12:06:31 +0300

    Добавил JwtResponse и LoginResponse, написал метод логина

--- src/main/java/com/simple_online_store_backend/config/SecurityConfig.java
+++ src/main/java/com/simple_online_store_backend/config/SecurityConfig.java
@@ -3,11 +3,15 @@ package com.simple_online_store_backend.config;
 import com.simple_online_store_backend.security.PersonDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,5 +34,16 @@ public class SecurityConfig {
                             return new AuthorizationDecision(isOwner);
                         }))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        // Создаем объект DaoAuthenticationProvider, который будет использоваться для аутентификации пользователей
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        // Устанавливаем сервис, который загружает информацию о пользователях (например, из базы данных)
+        authProvider.setUserDetailsService(userDetailsService);
+        // Указываем, какой кодировщик паролей использовать для сравнения введенного пароля с сохраненным
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authProvider);
     }
 }
---
+++ src/main/java/com/simple_online_store_backend/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.simple_online_store_backend.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
