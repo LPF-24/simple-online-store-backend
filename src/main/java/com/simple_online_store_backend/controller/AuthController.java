616bb202cd3850fc1319c34195641d4ff563c64b
max <mwlosev@gmail.com>
2025-03-15 12:06:31 +0300

    Добавил JwtResponse и LoginResponse, написал метод логина

--- src/main/java/com/simple_online_store_backend/controller/AuthController.java
+++ src/main/java/com/simple_online_store_backend/controller/AuthController.java
@@ -1,7 +1,48 @@
 package com.simple_online_store_backend.controller;
 
+import com.simple_online_store_backend.dto.JwtResponse;
+import com.simple_online_store_backend.dto.LoginRequest;
+import com.simple_online_store_backend.security.JWTUtil;
+import com.simple_online_store_backend.security.PersonDetails;
+import com.simple_online_store_backend.service.PersonDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final JWTUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final PersonDetailsService personDetailsService;
+
+    @GetMapping("/login")
+    public ResponseEntity<JwtResponse> authenticate(@RequestBody LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                            loginRequest.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+
+            String jwt = jwtUtil.generateToken(personDetails.getUsername());
+
+            return ResponseEntity.ok(new JwtResponse(jwt, personDetails.getId(), personDetails.getUsername()));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new JwtResponse(null, null, "Invalid username or password"));
+        }
+    }
 }
