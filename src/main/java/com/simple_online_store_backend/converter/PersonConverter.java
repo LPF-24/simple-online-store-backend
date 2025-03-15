8c20aa5d80b27a87a69e5a4d5c2cc6dd1b36244c
max <mwlosev@gmail.com>
2025-03-15 14:06:25 +0300

    Исправлена логика JWTFilter и JWTUtils, теперь role можно получить из токена, а не обращаясь к БД

---
+++ src/main/java/com/simple_online_store_backend/converter/PersonConverter.java
@@ -0,0 +1,4 @@
+package com.simple_online_store_backend.converter;
+
+public class PersonConverter {
+}
---
+++ src/main/java/com/simple_online_store_backend/dto/PersonRequestDTO.java
@@ -0,0 +1,42 @@
+package com.simple_online_store_backend.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PersonRequestDTO {
+    @NotEmpty(message = "Username can't be empty")
+    @Size(min = 2, max = 100, message = "Username must be between 2 and 100 characters long")
+    private String userName;
+
+    @NotEmpty(message = "Password can't be empty")
+    //временно
+    @Size(min = 8, max = 100, message = "Password must be at least 8 characters long")
+    //@ValidPassword
+    private String password;
+
+    //@ValidDateOfBirth
+    //временно
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    @Size(min = 2, max = 20, message = "Phone number must be between 2 and 20 characters long")
+    //временно
+    @Pattern(regexp = "\\+?[0-9\\- ]{7,20}", message = "Phone number format is invalid")
+    //@ValidPhoneNumber
+    private String phoneNumber;
+
+    @NotEmpty(message = "Password can't be empty")
+    @Email(message = "Email should be valid")
+    @Max(value = 50, message = "Email can contain a maximum of 50 characters")
+    private String email;
+
+    @NotNull(message = "You must accept the terms and conditions")
+    private Boolean agreementAccepted;
+}
