89727d898ed2ec5e8b19980edf501b3b31c31cc8
max <mwlosev@gmail.com>
2025-03-16 18:21:22 +0300

    Updated PersonConverter, PersonRequestDTO, PeopleService

--- src/main/java/com/simple_online_store_backend/mapper/PersonConverter.java
+++ src/main/java/com/simple_online_store_backend/mapper/PersonConverter.java
@@ -5,15 +5,23 @@ import com.simple_online_store_backend.dto.PersonResponseDTO;
 import com.simple_online_store_backend.entity.Person;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PersonConverter {
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public Person convertToPerson(PersonRequestDTO dto) {
         return modelMapper.map(dto, Person.class);
+    }
+
+    public Person convertToPersonToRequest(PersonRequestDTO dto) {
+        Person person = modelMapper.map(dto, Person.class);
+        person.setPassword(passwordEncoder.encode(dto.getPassword()));
+        return person;
     }
 
     public PersonResponseDTO convertToResponseDTO(Person person) {
