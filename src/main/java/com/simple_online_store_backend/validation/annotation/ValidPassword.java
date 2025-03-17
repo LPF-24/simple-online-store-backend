833fbe2d051e7def57ca777e1e3649a4d7ff91d9
max <mwlosev@gmail.com>
2025-03-17 13:31:11 +0300

    Добавлен ValidPassword и PasswordConstraintValidator

---
+++ src/main/java/com/simple_online_store_backend/validation/annotation/ValidPassword.java
@@ -0,0 +1,19 @@
+package com.simple_online_store_backend.validation.annotation;
+
+import com.simple_online_store_backend.validation.validator.PasswordConstraintValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "Invalid password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
