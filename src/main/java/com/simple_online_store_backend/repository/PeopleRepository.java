43c9c6086b691b39cf76d39d81e67b3a51dc996b
max <mwlosev@gmail.com>
2025-03-22 15:21:00 +0300

    Добавлен AddressUpdateDTO, добавлен метод updateAddress в AddressController, добавлен метод findAddressById в PeopleRepository, добавлен метод updateAddress в AddressService, метод getAddressIdByPersonId в PeopleService

--- src/main/java/com/simple_online_store_backend/repository/PeopleRepository.java
+++ src/main/java/com/simple_online_store_backend/repository/PeopleRepository.java
@@ -1,5 +1,6 @@
 package com.simple_online_store_backend.repository;
 
+import com.simple_online_store_backend.entity.Address;
 import com.simple_online_store_backend.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
     Optional<Person> findByUserName(String userName);
     List<Person> findAllByRole(String role);
+    Optional<Address> findAddressById(int addressId);
 }
