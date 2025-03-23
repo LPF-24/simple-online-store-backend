0a129ee843b9a8d03089cf342fbb2ea781766f4f
max <mwlosev@gmail.com>
2025-03-23 16:55:05 +0300

    Добавлен метод restoreAccount в PeopleController и PeopleService, добавлен блок проверки удаления аккаунта в методе login

--- src/main/java/com/simple_online_store_backend/controller/AuthController.java
+++ src/main/java/com/simple_online_store_backend/controller/AuthController.java
@@ -16,12 +16,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -40,7 +43,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> authenticate(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticate(@RequestBody LoginRequest loginRequest) {
         try {
             Authentication authentication = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
@@ -56,6 +59,9 @@ public class AuthController {
             String jwt = jwtUtil.generateToken(personDetails.getUsername(), role);
 
             return ResponseEntity.ok(new JwtResponse(jwt, personDetails.getId(), personDetails.getUsername()));
+        } catch (DisabledException e) {
+            return ResponseEntity.status(HttpStatus.LOCKED)
+                    .body(Map.of("message", "Your account is deactivated. Would you like to restore it?"));
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(new JwtResponse(null, null, "Invalid username or password"));
