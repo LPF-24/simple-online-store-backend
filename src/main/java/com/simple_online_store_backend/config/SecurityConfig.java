8fdd0781943d4bbeac38d55b43ff1b61746405a0
max <mwlosev@gmail.com>
2025-03-28 15:02:48 +0300

    Добавлен класс CorsConfig для настройки возможности фронтэнда обращаться к бэкэнду)

--- src/main/java/com/simple_online_store_backend/config/SecurityConfig.java
+++ src/main/java/com/simple_online_store_backend/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -71,6 +72,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
                                                    DaoAuthenticationProvider daoAuthenticationProvider) throws Exception {
         return http
+                .cors(Customizer.withDefaults()) // включение настройки межсайтовых запросов вручную
                 .csrf(AbstractHttpConfigurer::disable) // отключаем CSRF
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
