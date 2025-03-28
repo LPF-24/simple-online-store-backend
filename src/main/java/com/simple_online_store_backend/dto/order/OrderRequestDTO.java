f70e648f70c5dc0bb94ff4bfe7a433eb9d4bba3e
max <mwlosev@gmail.com>
2025-03-28 19:21:44 +0300

    Добавлен класс OrderRequestDTO, аннотации AccountActive и OrderDeliveryValidator, классы валидаторы AccountActiveValidator и OrderDeliveryValidator

--- src/main/java/com/simple_online_store_backend/dto/order/OrderRequestDTO.java
+++ src/main/java/com/simple_online_store_backend/dto/order/OrderRequestDTO.java
@@ -1,4 +1,31 @@
 package com.simple_online_store_backend.dto.order;
 
+import com.simple_online_store_backend.entity.Address;
+import com.simple_online_store_backend.entity.Person;
+import com.simple_online_store_backend.entity.PickupLocation;
+import com.simple_online_store_backend.enums.OrderStatus;
+import com.simple_online_store_backend.validation.annotation.AccountActive;
+import com.simple_online_store_backend.validation.annotation.ValidDeliveryOption;
+import com.simple_online_store_backend.validation.annotation.ValidPickupLocation;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ValidDeliveryOption
 public class OrderRequestDTO {
+    /*@NotNull(message = "Order status is required")
+    private OrderStatus status;*/
+
+    @NotNull(message = "An order cannot be placed without a customer")
+    @AccountActive
+    private Person person;
+
+    @ValidPickupLocation
+    private PickupLocation pickupLocation;
+
+    private Address address;
 }
