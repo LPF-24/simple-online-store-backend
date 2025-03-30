b95090ec9ca862a44891e7879cc9e1ef28187b8a
max <mwlosev@gmail.com>
2025-03-30 18:46:31 +0300

    Исправлен метод добавления заказов, теперь добавляются товары

--- src/main/java/com/simple_online_store_backend/dto/product/ProductShortDTO.java
+++ src/main/java/com/simple_online_store_backend/dto/product/ProductShortDTO.java
@@ -1,4 +1,18 @@
 package com.simple_online_store_backend.dto.product;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProductShortDTO {
+    private Integer id;
+
+    private String productName;
+
+    private BigDecimal price;
 }
--- src/main/java/com/simple_online_store_backend/service/OrderService.java
+++ src/main/java/com/simple_online_store_backend/service/OrderService.java
@@ -4,11 +4,14 @@ import com.simple_online_store_backend.dto.order.OrderRequestDTO;
 import com.simple_online_store_backend.dto.order.OrderResponseDTO;
 import com.simple_online_store_backend.entity.Order;
 import com.simple_online_store_backend.entity.Person;
+import com.simple_online_store_backend.entity.Product;
 import com.simple_online_store_backend.enums.OrderStatus;
 import com.simple_online_store_backend.exception.ValidationException;
 import com.simple_online_store_backend.mapper.OrderMapper;
+import com.simple_online_store_backend.repository.AddressRepository;
 import com.simple_online_store_backend.repository.OrderRepository;
 import com.simple_online_store_backend.repository.PeopleRepository;
+import com.simple_online_store_backend.repository.ProductRepository;
 import com.simple_online_store_backend.security.PersonDetails;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +29,8 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
     private final PeopleRepository peopleRepository;
+    private final ProductRepository productRepository;
+    private final AddressRepository addressRepository;
 
     @Transactional
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -40,9 +45,24 @@ public class OrderService {
             throw new ValidationException("Your account is deactivated. Please restore your account before placing an order.");
         }
 
+        List<Product> products = dto.getProductsIds().stream()
+                .map(id -> productRepository.findById(id)
+                        .orElseThrow(() -> new ValidationException("Product with ID " + id + " not found")))
+                .toList();
+
+        if (products.stream().anyMatch(p -> !Boolean.TRUE.equals(p.getAvailability()))) {
+            throw new ValidationException("Some products are not available for order");
+        }
+
         Order order = orderMapper.mapRequestToOrder(dto);
+        if (dto.getAddress() != null) {
+            addressRepository.save(dto.getAddress());
+            order.setAddress(dto.getAddress());
+        }
+
         order.setPerson(owner);
         order.setStatus(OrderStatus.PENDING);
+        order.setProducts(products);
 
         orderRepository.save(order);
         return orderMapper.mapEntityToResponse(order);
