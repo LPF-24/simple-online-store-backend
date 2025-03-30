5fa500e520a415d6c15e82c701e857c31e5cfca2
max <mwlosev@gmail.com>
2025-03-30 17:58:26 +0300

    Обновлен файл .gitignore

---
+++ src/main/java/com/simple_online_store_backend/validation/annotation/ValidProductsList.java
@@ -0,0 +1,4 @@
+package com.simple_online_store_backend.validation.annotation;
+
+public @interface ValidProductsList {
+}
---
+++ src/main/java/com/simple_online_store_backend/validation/validator/ProductsValidator.java
@@ -0,0 +1,4 @@
+package com.simple_online_store_backend.validation.validator;
+
+public class ProductsValidator {
+}
