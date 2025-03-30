2e1b29359e85a00f6b06c34e0bf861f14c1084c9
max <mwlosev@gmail.com>
2025-03-30 18:46:31 +0300

    Исправлен метод добавления заказов, теперь добавляются товары

--- src/main/java/com/simple_online_store_backend/dto/order/OrderResponseDTO.java
+++ src/main/java/com/simple_online_store_backend/dto/order/OrderResponseDTO.java
@@ -4,11 +4,14 @@ import com.simple_online_store_backend.dto.PersonResponseDTO;
 import com.simple_online_store_backend.dto.PersonShortDTO;
 import com.simple_online_store_backend.dto.address.AddressResponseDTO;
 import com.simple_online_store_backend.dto.pickup_location.PickupLocationResponseDTO;
+import com.simple_online_store_backend.dto.product.ProductShortDTO;
 import com.simple_online_store_backend.entity.PickupLocation;
 import com.simple_online_store_backend.enums.OrderStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -17,6 +20,8 @@ public class OrderResponseDTO {
     private OrderStatus status;
 
     private PersonShortDTO person;
+
+    private List<ProductShortDTO> products;
 
     private PickupLocationResponseDTO pickupLocation;
 
