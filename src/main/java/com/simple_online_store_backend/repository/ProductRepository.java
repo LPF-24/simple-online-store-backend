e1b2008072fa63d9177166107e415a0f24e441a1
max <mwlosev@gmail.com>
2025-04-04 19:43:11 +0300

    Added method to get products with active = true

--- src/main/java/com/simple_online_store_backend/repository/ProductRepository.java
+++ src/main/java/com/simple_online_store_backend/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.simple_online_store_backend.repository;
 import com.simple_online_store_backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findAllByAvailabilityTrue();
 }
