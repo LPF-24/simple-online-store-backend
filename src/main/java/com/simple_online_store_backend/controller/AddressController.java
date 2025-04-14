d4018ba829c66bf75bcc08ebeedb04c3c5a90d57
max <mwlosev@gmail.com>
2025-04-14 21:33:32 +0300

    add OpenAPI annotations to AddressController

--- src/main/java/com/simple_online_store_backend/controller/AddressController.java
+++ src/main/java/com/simple_online_store_backend/controller/AddressController.java
@@ -6,7 +6,10 @@ import com.simple_online_store_backend.exception.ErrorUtil;
 import com.simple_online_store_backend.security.PersonDetails;
 import com.simple_online_store_backend.service.AddressService;
 import com.simple_online_store_backend.service.PeopleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Address", description = "Endpoints for work with address")
 @RequestMapping("/address")
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +26,13 @@ public class AddressController {
     private final AddressService addressService;
     private final PeopleService peopleService;
 
+    @Operation(summary = "Add an address", description = "Adds an address for the user")
+    @ApiResponse(responseCode = "200", description = "Successfully added")
+    @ApiResponse(responseCode = "500", description = "Error inside method")
+    @ApiResponse(responseCode = "422", description = "Request is syntactically correct but semantically invalid")
+    @ApiResponse(responseCode = "403", description = "User is authenticated but not allowed to access this resource")
+    @ApiResponse(responseCode = "400", description = "Request is invalid or missing required parameters")
+    @ApiResponse(responseCode = "422", description = "Validation failed on request data")
     @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/add-address")
     public ResponseEntity<AddressResponseDTO> addAddress(@RequestBody @Valid AddressRequestDTO dto,
@@ -35,6 +46,12 @@ public class AddressController {
         return ResponseEntity.ok(address);
     }
 
+    @Operation(summary = "Update an address", description = "Updates an address for the user")
+    @ApiResponse(responseCode = "200", description = "Successfully updated")
+    @ApiResponse(responseCode = "500", description = "Error inside method")
+    @ApiResponse(responseCode = "422", description = "Request is syntactically correct but semantically invalid")
+    @ApiResponse(responseCode = "403", description = "User is authenticated but not allowed to access this resource")
+    @ApiResponse(responseCode = "400", description = "Request is invalid or missing required parameters")
     @SecurityRequirement(name = "bearerAuth")
     @RequestMapping(value = "/update-address", method = {RequestMethod.PATCH, RequestMethod.POST})
     public ResponseEntity<AddressResponseDTO> updateAddress(@RequestBody @Valid AddressRequestDTO dto,
